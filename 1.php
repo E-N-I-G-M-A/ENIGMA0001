d4366f559ad433327b50720fee73bada34d09f0b
carlosguilhermeklein <84053199+carlosguilhermeklein@users.noreply.github.com>
2022-01-07 16:23:01 -0300

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <link rel="stylesheet" type="text/css" href="stilos .css"/>
+  <!--  meu tu ta cedi de mais aqui mas foi uma boa tentativa -->
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>todo filme foda começa com uma tela preta</title>
+
+   
+</head>
+<body>
+    
+</body>
+</html>
